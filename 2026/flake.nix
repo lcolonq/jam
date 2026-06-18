cd03974523bd7c46ca32c07a1b145e1710218b44
LLLL Colonq <llll@colonq>
2026-06-18 19:26:09 -0400

    Add nonnix build

--- 2026/flake.nix
+++ 2026/flake.nix
@@ -10,9 +10,22 @@
       pkgs = import nixpkgs { inherit system; };
       jam = inputs.teleia.native.build ./. "jam";
       windows = inputs.teleia.windows.build ./. "jam";
+      nonnix = pkgs.stdenv.mkDerivation {
+        name = "jam-nonnix";
+        phases = [ "installPhase" ];
+        installPhase = ''
+          mkdir $out
+          cp -rv ${jam}/bin/jam_server $out
+          chmod +w $out/jam_server
+          patchelf --remove-rpath $out/jam_server
+          patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/jam_server
+          strip $out/jam_server
+          chmod -w $out/jam_server
+        '';
+      };
     in {
       packages.${system} = {
-        inherit jam windows;
+        inherit jam windows nonnix;
         default = jam;
       };
       applications.${system}.default = {
