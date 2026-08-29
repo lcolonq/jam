58070f6c79c4dd5b3a32fea7d18a0acf8742f08b
LLLL Colonq <llll@colonq>
2026-08-29 05:42:58 -0400

    Add rpc2dot0 pkg

---
+++ 2026/games/rpc2dot0/pkg/eatyourgreens_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const get_result_display_delay_ms: () => number;
+export const main_js: (a: number) => void;
+export const receive_op_js: (a: number, b: number) => void;
+export const set_difficulty_js: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure4_externref_shim: (a: number, b: number, c: any) => void;
+export const closure7_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure488_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core_34211b08701b9ee6___ops__function__FnMut_____Output______as_wasm_bindgen_2b4b1ea8acc7b00___closure__WasmClosure___describe__invoke______: (a: number, b: number) => void;
+export const closure564_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
